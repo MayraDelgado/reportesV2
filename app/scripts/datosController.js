c8f20bb99a68d1120eb1dc8a866181901ed9e8e1
MayraDelgado <31661546+MayraDelgado@users.noreply.github.com>
2018-08-10 14:38:55 -0500

    Update datosController.js

--- app/scripts/datosController.js
+++ app/scripts/datosController.js
@@ -18,7 +18,7 @@ app.controller('accesoDatosController', ['$scope', '$filter', '$http', '$mdSelec
     api.call("Get", {
         "typeName": "Device",
         "search": {
-        "name": "37311-5"}
+        "name": "27564-0"}
     }, function (result) {
         $scope.lstDeviceGeotab = result;
         $scope.lstDeviceGeotab.forEach(function (device) {
